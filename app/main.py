e5e419eea718b9725b714d371eedd528f83b9f21
min_cloud <purione1@naver.com>
2025-06-19 18:35:57 +0900

    FIX: test branch main.py
    CORS 문제 해결

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.api.v1.routers import auth, story
 from app.AI import router as ai_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
