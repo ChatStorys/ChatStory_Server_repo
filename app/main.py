cbc3db3cdbbc4c2fd89c62afa07b19b9a909f244
min_cloud <purione1@naver.com>
2025-06-19 18:43:52 +0900

    FIX: main.py
    CORS 설정

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=[os.getenv("FRONTEND_ORIGIN")],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
