636b8f4335bdd545914955434d008b5d37404bbe
junyongleee <1dlwnsdyd1@gmail.com>
2025-06-21 11:38:06 +0900

    벡, 웹 cors 이슈 해결

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://DBadmin:Fxz6FChRFep4NCvb@chatstorys.adsotkt.mongodb.net/?retryWrites=true&w=majority&appName=ChatStorys")
+MONGO_URL = os.getenv("MONGO_URL")
 DB_NAME = os.getenv("DB_NAME", "ChatStorysUser")
 
 client = MongoClient(MONGO_URL)
