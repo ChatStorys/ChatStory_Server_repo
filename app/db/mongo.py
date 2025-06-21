cc8295242f69f3c228d33441e2583f97cf3c3be9
min_cloud <purione1@naver.com>
2025-06-21 10:16:47 +0900

    FIX: mongo.py
    env 파일 확인점...

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,8 +1,12 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://DBadmin:Fxz6FChRFep4NCvb@chatstorys.adsotkt.mongodb.net/?retryWrites=true&w=majority&appName=ChatStorys")
-DB_NAME = os.getenv("DB_NAME", "ChatStorysUser")
+# .env 파일 로드
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME", "ChatStorysUser")  
 
 client = MongoClient(MONGO_URL)
 db = client[DB_NAME]
