fbc6876f1cb0a21caad38836968c212724ae44be
min_cloud <purione1@naver.com>
2025-06-21 13:18:52 +0900

    FIX: main.py
    expose_handler부분

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Authorization"],
 )
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
